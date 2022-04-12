2d418658582d3219dd70efc9b1fc2725c0d6d4e3
JJH <jjhowlett0595@gmail.com>
2022-04-12 13:51:45 -0400

    Add gitignore

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, CreateUserForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+from dotenv import load_dotenv
+import os
+
+
+# Load environment variables from .env
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
